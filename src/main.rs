1ffb70e440cd70eba7acd082fe63d69c34dc2289
Cahya Wirawan <cahya.wirawan@gmail.com>
2024-06-10 16:27:01 +0200

    move the time measurement

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod utils;
 use utils::tokenizer;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+use std::time::Duration;
 use serde::Deserialize;
 
 
@@ -15,12 +16,14 @@ fn main() {
     let tokenizer = tokenizer::Tokenizer::new("data/rwkv_vocab_v20230424.txt").unwrap();
     let file = File::open("data/wiki-en.jsonl").expect("couldn't open file");
     use std::time::Instant;
-    let now = Instant::now();
     let mut counter = 0;
+    let mut elapsed: Duration = Duration::new(0, 0);
     for line in BufReader::new(file).lines() {
         let line = line.expect("couldn't get line");
         let ds: Jsonline = serde_json::from_str(&line).unwrap();
+        let now = Instant::now();
         let encode = tokenizer.encode(ds.text.as_str());
+        elapsed += now.elapsed();
         counter += encode.len();
         /*
         let decode = tokenizer.decode(encode.to_vec());
@@ -33,7 +36,6 @@ fn main() {
         index += 1;
         */
     }
-    let elapsed = now.elapsed();
     // println!("Index: {:?}", index);
     println!("Number of tokens: {:?}", counter);
     println!("Elapsed time: {:.2?}", elapsed);
